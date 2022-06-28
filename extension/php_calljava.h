2f212597d9722b6367a53e7dcb053811239983d2
tedo0627 <24734045+tedo0627@users.noreply.github.com>
2022-06-29 08:52:44 +0900

    Replaced by c++

--- extension/php_calljava.h
+++ extension/php_calljava.h
@@ -1,15 +1,13 @@
-/* calljava extension for PHP */
-
 #ifndef PHP_CALLJAVA_H
-# define PHP_CALLJAVA_H
+#define PHP_CALLJAVA_H
 
 extern zend_module_entry calljava_module_entry;
-# define phpext_calljava_ptr &calljava_module_entry
+#define phpext_calljava_ptr &calljava_module_entry
 
-# define PHP_CALLJAVA_VERSION "0.1.0"
+#define PHP_CALLJAVA_VERSION "0.1.0"
 
-# if defined(ZTS) && defined(COMPILE_DL_CALLJAVA)
+#if defined(ZTS) && defined(COMPILE_DL_CALLJAVA)
 ZEND_TSRMLS_CACHE_EXTERN()
-# endif
+#endif
 
-#endif	/* PHP_CALLJAVA_H */
+#endif
