2f5790f21b43ebc64620af2f6ade3d732c7f50c3
tedo0627 <24734045+tedo0627@users.noreply.github.com>
2022-06-29 11:42:49 +0900

    Fix indent

--- extension/calljava.cpp
+++ extension/calljava.cpp
@@ -3,39 +3,39 @@
 #endif
 
 extern "C" {
-    #include "php.h"
-    #include "ext/standard/info.h"
-    #include "php_calljava.h"
+#include "php.h"
+#include "ext/standard/info.h"
+#include "php_calljava.h"
 }
 
 PHP_MINIT_FUNCTION(calljava) {
-	return SUCCESS;
+    return SUCCESS;
 }
 
 PHP_MINFO_FUNCTION(calljava) {
-	php_info_print_table_start();
-	php_info_print_table_header(2, "calljava support", "enabled");
-	php_info_print_table_end();
+    php_info_print_table_start();
+    php_info_print_table_header(2, "calljava support", "enabled");
+    php_info_print_table_end();
 }
 
 zend_module_entry calljava_module_entry = {
-	STANDARD_MODULE_HEADER,
-	"calljava",
-	NULL,
-	PHP_MINIT(calljava),
-	NULL,
-	NULL,
-	NULL,
-	PHP_MINFO(calljava),
-	PHP_CALLJAVA_VERSION,
-	STANDARD_MODULE_PROPERTIES
+    STANDARD_MODULE_HEADER,
+    "calljava",
+    NULL,
+    PHP_MINIT(calljava),
+    NULL,
+    NULL,
+    NULL,
+    PHP_MINFO(calljava),
+    PHP_CALLJAVA_VERSION,
+    STANDARD_MODULE_PROPERTIES
 };
 
 #ifdef COMPILE_DL_CALLJAVA
 extern "C" {
-    #ifdef ZTS
+#ifdef ZTS
     ZEND_TSRMLS_CACHE_DEFINE()
-    #endif
+#endif
     ZEND_GET_MODULE(calljava)
 }
 #endif
