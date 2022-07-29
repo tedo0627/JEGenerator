7ac30f4cbd136865e83d22b1c3a5cceace78a6b6
tedo0627 <24734045+tedo0627@users.noreply.github.com>
2022-07-29 16:27:51 +0900

    Remove optional argument

--- extension/stubs/tedo0627/jegenerator/extension/JELoader_arginfo.h
+++ extension/stubs/tedo0627/jegenerator/extension/JELoader_arginfo.h
@@ -8,10 +8,10 @@ ZEND_END_ARG_INFO()
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_tedo0627_jegenerator_extension_JELoader_init, 0, 0, _IS_BOOL, 0)
 ZEND_END_ARG_INFO()
 
-ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX(arginfo_tedo0627_jegenerator_extension_JELoader_getGenerator, 0, 2, tedo0627\\jegenerator\\extension\\JEGenerator, 0)
+ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX(arginfo_tedo0627_jegenerator_extension_JELoader_getGenerator, 0, 3, tedo0627\\jegenerator\\extension\\JEGenerator, 0)
     ZEND_ARG_TYPE_INFO(0, type, IS_STRING, 0)
     ZEND_ARG_TYPE_INFO(0, seed, IS_LONG, 0)
-    ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, type, IS_STRING, 0, "")
+    ZEND_ARG_TYPE_INFO(0, biome, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_METHOD(tedo0627_jegenerator_extension_JELoader, __construct);
