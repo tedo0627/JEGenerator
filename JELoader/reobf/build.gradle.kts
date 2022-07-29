907ca9c46e863c3131314b1ac37b5fd423437a7c
tedo0627 <24734045+tedo0627@users.noreply.github.com>
2022-07-29 21:08:38 +0900

    Use gradle multi project

---
+++ JELoader/reobf/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.6.20"
+    java
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "jp.tedo0627.jeloader.reobf"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.javassist:javassist:3.29.0-GA")
+    implementation(fileTree(mapOf("dir" to "lib", "include" to "server.jar")))
+}
+
+tasks {
+    shadowJar {
+        exclude("server.jar")
+    }
+}
